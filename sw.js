3fb18a6763ec320d70a3d9e381861dbd2b547118
kulkankaraeski  <kulkankaraeski@gmail.com>
2026-04-24 15:08:02 +0300

    bağlantılar

--- sw.js
+++ sw.js
@@ -3,7 +3,10 @@ const STATIC_ASSETS = [
     './',
     './index.html',
     './manifest.json',
-    './icon-512.png'
+    './icon-512.png',
+    'https://cdn.tailwindcss.com',
+    'https://cdn.jsdelivr.net/npm/chart.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
 ];
 
 self.addEventListener('install', event => {
